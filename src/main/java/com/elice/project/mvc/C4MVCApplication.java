89459a9b67543f1fb14a4682de6b7db57c1627f5
bada <silverzoo030@gmail.com>
2024-08-05 18:18:14 +0900

    :sparkles: ch0701 - mvc with html/css

---
+++ src/main/java/com/elice/project/mvc/C4MVCApplication.java
@@ -0,0 +1,14 @@
+package com.elice.project.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C4MVCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C4MVCApplication.class, args);
+
+    }
+
+}
---
+++ src/main/java/com/elice/project/mvc/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.elice.project.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    public String getIndexPage(){
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String getHomePage(){
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String getAboutPage(){
+        return "about";
+    }
+
+}
