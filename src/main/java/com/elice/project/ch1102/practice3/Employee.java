43dae1fbf51d2fff5d3372d818f5e1e1f8e226af
bada <silverzoo030@gmail.com>
2024-09-14 21:15:31 +0900

    :sparkles: ch1102 - 단방향 @ManyToOne

---
+++ src/main/java/com/elice/project/ch1102/practice3/Employee.java
@@ -0,0 +1,21 @@
+package com.elice.project.ch1102.practice3;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    // 지시사항을 참고하여 코드를 작성해 보세요.
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
